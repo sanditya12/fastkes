382c072bf70ae9579ad83bee8c9f4a110002e450
dfakhrizaman <daffafakhrizaman@gmail.com>
2021-07-22 01:33:46 +0700

    Changed readme

--- lib/pages/pick_hospital_page.dart
+++ lib/pages/pick_hospital_page.dart
@@ -2,13 +2,13 @@ import 'package:fastkes_app/widgets/search_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../widgets/transparent_appbar.dart';
-// import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class PickHospitalPage extends StatelessWidget {
   final Color dividerColor = Color(0xff636464);
 
-  // static const _initialCameraPosition = CameraPosition(
-  //     target: LatLng(-6.389965886184355, 106.83993210880122), zoom: 11.5);
+  static const _initialCameraPosition = CameraPosition(
+      target: LatLng(-6.389965886184355, 106.83993210880122), zoom: 11.5);
 
   @override
   Widget build(BuildContext context) {
@@ -21,19 +21,19 @@ class PickHospitalPage extends StatelessWidget {
         children: [
           Expanded(
             flex: 1,
-            child: Container(
-              width: double.infinity,
-              color: Colors.green[400],
-              child: Text('Map'),
-            ),
-            // child: GoogleMap(
-            //   initialCameraPosition: _initialCameraPosition,
-            //   myLocationButtonEnabled: false,
-            //   zoomControlsEnabled: false,
+            // child: Container(
+            //   width: double.infinity,
+            //   color: Colors.green[400],
+            //   child: Text('Map'),
             // ),
+            child: GoogleMap(
+              initialCameraPosition: _initialCameraPosition,
+              myLocationButtonEnabled: false,
+              zoomControlsEnabled: false,
+            ),
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 48),
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
             child: SearchBar(),
           ),
           Expanded(
